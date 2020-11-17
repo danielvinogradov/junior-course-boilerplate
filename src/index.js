b790570a333f28eaf50557f2056fef3c71da3fb9
Daniel Vinogradov <46866852+danielvinogradov@users.noreply.github.com>
2020-11-17 16:13:23 +0300

    Добавляет index.js в src. Рендерит компонент App в #root

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+import './assets/css/index.css'
+
+const rootNode = document.querySelector('#root')
+
+ReactDOM.render(<App/>, rootNode)
