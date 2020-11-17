ae036191557f097f9bcfb1d28407942e1dd8c191
Daniel Vinogradov <46866852+danielvinogradov@users.noreply.github.com>
2020-11-17 16:12:16 +0300

    Добавляет компонент App

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import BaseHeadline from './components/BaseHeadline/BaseHeadline'
+import BaseList from './components/BaseList/BaseList'
+
+import listItems from './data/products.json'
+
+const data = { headline: 'Список товаров', listItems, }
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <BaseHeadline headline={data.headline}/>
+        <BaseList listItems={data.listItems}/>
+      </div>
+    )
+  }
+}
+
+export default App
