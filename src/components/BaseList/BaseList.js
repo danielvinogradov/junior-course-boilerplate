aae16af6f210eb3775405fe6fb58996604bf80f1
Daniel Vinogradov <46866852+danielvinogradov@users.noreply.github.com>
2020-11-18 20:49:32 +0300

    Создает props.maxItemsToShow в BaseList и удаляет объявление одноименной константы

--- src/components/BaseList/BaseList.js
+++ src/components/BaseList/BaseList.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const params = { maxItemsToShow: 3 }
-
 class BaseList extends React.Component {
   render() {
-    if (this.props.listItems && params.maxItemsToShow > 1) {
+    if (this.props.listItems && this.props.maxItemsToShow > 0) {
       return (
         <ul>
-          {this.props.listItems.slice(0, params.maxItemsToShow).map(item => <li key={item.id}>{item.name}</li>)}
+          {this.props.listItems.slice(0, this.props.maxItemsToShow).map(item => <li key={item.id}>{item.name}</li>)}
         </ul>
       )
-    } else if (params.maxItemsToShow < 1) {
-      return <p>Ошибка конфигурации: <code>params.maxItemsToShow</code> меньше 1</p>
     } else {
       return <p>Отсутствуют элементы списка</p>
     }
@@ -20,10 +16,34 @@ class BaseList extends React.Component {
 }
 
 BaseList.propTypes = {
+  /**
+   * Массив элементов списка
+   * @array
+   * @param {number} id
+   * @param {string} name
+   */
   listItems: PropTypes.arrayOf(PropTypes.exact({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-  }))
+  })),
+
+  /**
+   * Максимальное количество элементов списка для отображения.
+   * Не может быть меньше 1.
+   * @number
+   */
+  maxItemsToShow: (props, propName, componentName) => {
+    if (typeof (props[propName]) !== 'number') {
+      return new Error(`Invalid prop \`${propName}\` supplied to \`${componentName}\`. Expecting a number, but \`${propName}\` is a ${typeof (props[propName])} Validation failed.`);
+    }
+    if (props[propName] < 1) {
+      return new Error(`Invalid prop \`${propName}\` supplied to \`${componentName}\`. Value of \`${propName}\` should be greater than 0, but it is equal to ${props[propName]}. Validation failed.`);
+    }
+  }
+}
+
+BaseList.defaultProps = {
+  maxItemsToShow: 3
 }
 
 export default BaseList
